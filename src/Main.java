220d02acbbd484cd9cb146dfd9945e1d7460909c
korko <korkos@list.ru>
2022-09-09 14:06:39 +0300

    коммит 7

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,54 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        //task 1
+        String fistName = "Ivan";
+        String middlName = "Ivanovich";
+        String lastName = "Ivanov";
 
-        System.out.println("Hello world!");
+        String fullName = lastName + " " + fistName + " " + middlName;
+        System.out.println(" ФИО сотрудника - " + fullName);
+    }
+    
+    public static void task2(){
+        var fistName="Ivan";
+        var middleName="Ivanovich";
+        var lastName="Ivanov";
+
+        var fullName= String.format("%S %s %s", fistName, middleName, lastName);
+        var result = fullName.toUpperCase();
+        System.out.printf("Данные ФИО сотрудника для заполнения отчета - %s", fullName.toUpperCase());
+
+    }
+    public static void task3(){
+        var fullName = "Иванов Семён Семёнович";
+        var split  = fullName.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            System.out.println( split[i]);
+        }
+        
+        for (Spring current : split) {
+            if (current.contains ("ё"))
+                var text = current.replace 
+            System.out.println(current);
+        }
+        System.out.println(fullName);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
